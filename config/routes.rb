332c3fbcc3cfce1e552a25e30ada16b0b1e335ba
grantkee <grant.d.kee@gmail.com>
2021-02-17 11:24:07 -0600

    added resource :expenses to routes and updated controller with all actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'expenses/index'
   root 'landing#index'
+
+  resources :expenses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
