7a4187ccb3e95c299c6fe1d3a439e873fec945ab
grantkee <grant.d.kee@gmail.com>
2021-02-17 11:45:04 -0600

    checking params in expenses controller for create action

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,17 +1,27 @@
 class ExpensesController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
+    @expenses = Expense.all
   end
 
   def show
+    @expense = Expense.find(params[:id])
+  end
+  
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(name: "school", budget_total: 123.45, current_total: 0.00, color: "red", icon: "1", history: "")
+
+    if @expense.save
+      redirect_to @expense
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
   end
 
   def update
@@ -19,5 +29,9 @@ class ExpensesController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+    def expense_params
+      params.require(:expense).permit(:name, :budget_total, :current_total, :color, :icon)
+    end
 end
--- db/migrate/20210217165116_create_expenses.rb
+++ db/migrate/20210217165116_create_expenses.rb
@@ -2,11 +2,11 @@ class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
       t.string :name, null: false
-      t.decimal :budget_total, null: false
-      t.decimal :current_total, null: false
+      t.decimal :budget_total, null: false, precision: 7, scale: 2
+      t.decimal :current_total, null: false, precision: 7, scale: 2
       t.string :color, null: false
       t.string :icon, null: false
-      t.string :name, null: false
+      # t.string :name, null: false
       t.timestamps
     end
   end
