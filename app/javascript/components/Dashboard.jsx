11e51baa1c10787850c1a4dc94ed02cd45ffbbfb
grantkee <grant.d.kee@gmail.com>
2021-02-16 21:24:44 -0600

    created home page with material-ui

--- app/javascript/components/Dashboard.jsx
+++ app/javascript/components/Dashboard.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
 
 const Dashboard = () => (
-  <div className="homepage-container">
-    <div className="jumbotron">
-      <Typography align="center" variant="h1">
-        Personal Finance
-      </Typography>
-      <Typography align="center" variant="body1">
-        Good judgement comes from experience. Experience comes from bad judgement.
-      </Typography>
-    </div>
+  <div>
+    Hiii
   </div>
 );
 
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,25 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 const Home = () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Personal Finance</h1>
-        <p className="lead">
-          Good judgement comes from experience. Experience comes from bad judgement.
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/dashboard"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          My Finances
-        </Link>
-      </div>
-    </div>
-  </div>
+  <Grid container justify="center" alignItems="center" xs={6}>
+    <Grid item xs={12}>
+      <Typography align="center" variant="h1">
+        Personal Finance
+      </Typography>
+      <Typography align="center" variant="body1">
+        Good judgement comes from experience. Experience comes from bad judgement.
+      </Typography>
+      <Divider />
+    </Grid>
+    <Grid item xs={12}>
+      <Link
+        to="/dashboard"
+        className="btn btn-lg custom-button"
+        role="button"
+      >
+        My Finances
+      </Link>
+    </Grid>
+  </Grid>
 );
 
 export default Home;
