2f11c4f9cd34b3969c48db7d013fac0a1172f61b
grantkee <grant.d.kee@gmail.com>
2021-02-17 14:36:50 -0600

    destroy action made in transactions controller - updated expense model to destroy dependent transactions upon successful destruction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,6 +6,14 @@ class TransactionsController < ApplicationController
     redirect_to expense_path(@expense)
   end
 
+  def destroy
+    @expense = Expense.find(params[:expense_id])
+    @transaction = @expense.transactions.find(params[:id])
+    @transaction.destroy
+
+    redirect_to expense_path(@expense)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:amount)
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 
   validates :name, :budget_total, :current_total, :color, :icon, presence: true
 end
