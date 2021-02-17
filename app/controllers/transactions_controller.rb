64721f4166601105ca62f56bb912948ed354e13a
grantkee <grant.d.kee@gmail.com>
2021-02-17 14:09:47 -0600

    def create in transactions_controller ready for testing

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,13 @@
 class TransactionsController < ApplicationController
+  def create
+    @expense = Expense.find(params[:expense_id])
+    @transaction = @expense.transactions.create(comment_params)
+
+    redirect_to expense_path(@expense)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:amount)
+    end
 end
