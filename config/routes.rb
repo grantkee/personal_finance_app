72851b2d97001c967271628ac9674b91f69fa655
grantkee <grant.d.kee@gmail.com>
2021-02-17 13:31:39 -0600

    added transactions resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'landing#index'
 
-  resources :expenses
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :expenses do
+    resources :transactions
+  end
 end
