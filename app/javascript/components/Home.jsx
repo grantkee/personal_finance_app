ba1cd96d777631f912470345a3425d21bddf98cd
grantkee <grant.d.kee@gmail.com>
2021-02-16 19:36:32 -0600

    rough draft of home page using bootstrap

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Personal Finance</h1>
+        <p className="lead">
+          Good judgement comes from experience. Experience comes from bad judgement.
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/dashboard"
+          className="btn btn-lg custom-button"
+          role="button"
+        >
+          My Finances
+        </Link>
+      </div>
+    </div>
+  </div>
+);
