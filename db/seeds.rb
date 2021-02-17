0e1810cf5d0693d6e2ddf909feb7d2f1ac8aa6d3
grantkee <grant.d.kee@gmail.com>
2021-02-17 15:52:38 -0600

    added commas to seed file. db:seed successful

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 3.times do |i|
   Expense.create(
     name: "Expense #{i + 1}",
-    budget_total: 123.45
-    current_total: 0.00
-    color: "#{i}"
-    icon: "#{i + 1}"
+    budget_total: 123.45,
+    current_total: 0.00,
+    color: "#{i}",
+    icon: "#{i + 1}",
   )
 end
