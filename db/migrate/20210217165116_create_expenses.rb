7cd833b11cecd4e10dfcb743d707732954922293
grantkee <grant.d.kee@gmail.com>
2021-02-17 11:20:51 -0600

    updated some fields in expenses migration file

--- db/migrate/20210217165116_create_expenses.rb
+++ db/migrate/20210217165116_create_expenses.rb
@@ -1,7 +1,12 @@
 class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
-
+      t.string :name, null: false
+      t.decimal :budget_total, null: false
+      t.decimal :current_total, null: false
+      t.string :color, null: false
+      t.string :icon, null: false
+      t.string :name, null: false
       t.timestamps
     end
   end
