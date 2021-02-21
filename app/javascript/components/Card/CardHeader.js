777759a5b4719c141a07070d8006de5a1a1e076a
grantkee <grant.d.kee@gmail.com>
2021-02-21 13:54:29 -0600

    migrated card header

--- app/javascript/components/Card/CardHeader.js
+++ app/javascript/components/Card/CardHeader.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 
-import styles from '../../assets/jss/dashboard/components/cardHeaderStyle.js';
+import styles from '../../useStyles/Card/cardHeader';
 
 const useStyles = makeStyles(styles);
 
