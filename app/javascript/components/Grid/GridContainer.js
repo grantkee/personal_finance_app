b01bcfcd9f1555052507b2ad4112e25bc3350dc4
grantkee <grant.d.kee@gmail.com>
2021-02-20 18:23:07 -0600

    grid styles updated

--- app/javascript/components/Grid/GridContainer.js
+++ app/javascript/components/Grid/GridContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles'
-import styles from '../../useStyles/Grid/Grid';
+import styles from '../../useStyles/Grid/grid';
 
 const useStyles = makeStyles(styles);
 
