6bbffe8ea3b8ab297bc52eda651a7566ff1022b0
grantkee <grant.d.kee@gmail.com>
2021-02-22 16:38:58 -0600

    migrated card carousel

---
+++ app/javascript/components/Card/CardCarousel.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+// import styles from '../../useStyles/Card/cardCarousel';
+import ArrowBody from './Rotation/ArrowBody';
+import NextBill from './Rotation/NextBill';
+import NextPaycheck from './Rotation/NextPaycheck';
+
+// const useStyles = makeStyles(styles);
+
+const CardCarousel = (props) => {
+  // const classes = useStyles();
+
+  const { color } = props;
+  const [rotation, setRotation] = useState(1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRotation(rotation + 1);
+    }, 5*1000);
+  }, [rotation]);
+
+  const rotateContent = () => {
+    const num = rotation % 3;
+    switch(num) {
+      case 0:
+        return <ArrowBody color={color} />;
+      case 1:
+        return <NextBill color={color} />;
+      case 2:
+        return <NextPaycheck color={color} />;
+      default:
+        <p>Something Went Wrong??</p>
+    }
+  };
+
+  return (
+    <>
+      {rotateContent()}
+    </>
+  );
+};
+
+CardCarousel.propTypes = {
+  color: PropTypes.string,
+};
+
+export default CardCarousel;
---
+++ app/javascript/useStyles/Card/cardCarousel.js
@@ -0,0 +1,5 @@
+const cardCarousel = {
+  
+};
+
+export default cardCarousel;
