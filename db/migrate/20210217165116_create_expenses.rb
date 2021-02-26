acbc5f96931accdcd16066920358ac0a7d84cb7d
grantkee <grant.d.kee@gmail.com>
2021-02-25 22:06:23 -0600

    added boolean column :income

--- db/migrate/20210217165116_create_expenses.rb
+++ db/migrate/20210217165116_create_expenses.rb
@@ -1,11 +1,12 @@
 class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
-      t.string :name, null: false
-      t.decimal :budget_total, null: false, precision: 7, scale: 2
+      t.string  :name,          null: false
+      t.decimal :budget_total,  null: false, precision: 7, scale: 2
       t.decimal :current_total, null: false, precision: 7, scale: 2
-      t.string :color, null: false
-      t.string :icon, null: false
+      t.string  :color,         null: false
+      t.string  :icon,          null: false
+      t.boolean :income         null: false, default: false
 
       t.timestamps
     end
