9ac4352c3b50d61f9fed48bfd56ab33fa56c2a86
grantkee <grant.d.kee@gmail.com>
2021-10-23 12:23:35 -0500

    rails g rspec:install

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,19 @@
-require 'spec_helper'
+require 'rails_helper'
 
-Rspec.describe "Category" do
+RSpec.describe Category, type: :model do
   before do
-    cat = Category.new("")
+    cat = Category.new("lifestyle")
   end
+
+  # use when, with, or without for context
+  context ".name" do
+    it "should return category name" do
+      expect(cat.name).to eq("lifestyle")
+    end
+  end
+
+  # use when, with, or without for context
+  # context "when given a name"
+    # it {is_expected.to }
+  # end
 end
