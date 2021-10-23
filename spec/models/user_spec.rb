7ea7e50b56db5ec2bbe064821e6fb7324d541ed0
grantkee <grant.d.kee@gmail.com>
2021-10-23 14:59:20 -0500

    update model/user_spec to check associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
     )
   }
 
-  describe "Validations" do
+  context "Validations" do
     it "is valid with valid attributes" do
       expect(subject).to be_valid
     end
@@ -22,5 +22,9 @@ RSpec.describe User, type: :model do
       subject.email = nil
       expect(subject).to_not be_valid
     end
+  end
+
+  context "Associations" do
+    it { should have_many(:categories).without_validating_presence }
   end
 end
