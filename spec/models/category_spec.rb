dd5b108eb15f337fe3bf1ac3e1b289180b553a1e
grantkee <grant.d.kee@gmail.com>
2021-10-23 14:38:49 -0500

    category spec updated with validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,8 +9,11 @@ require 'rails_helper'
 #   Can I implement something more efficiently?
 
 RSpec.describe Category, type: :model do
+  let(:user) {
+    User.create(email: "batman@cave.com", password: "pw1234")
+  }
   subject {
-    described_class.new(name: "lifestyle")
+    described_class.new(id: 1, name: "lifestyle", user_id: user.id)
   }
 
   context "when creating a new instance" do
@@ -34,6 +37,11 @@ RSpec.describe Category, type: :model do
     it "icon is 0 by default" do
       expect(subject.icon).to eq(0)
     end
+  end
+
+  context "Associations" do
+    it { should belong_to(:user) }
+    it { should have_many(:expenses) }
   end
 
 end
