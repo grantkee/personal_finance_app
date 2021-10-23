7a9026959d05258c41c6e54df4e747ddf5ba069a
grantkee <grant.d.kee@gmail.com>
2021-10-23 13:02:42 -0500

    rspec tests passing. time for refactor of tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,14 +1,37 @@
 require 'rails_helper'
 
+# While "in the green" and before moving back to "in the red" consider:
+#   Can I make my test suite more expressive?
+#   Does my test suite provide reliable feedback?
+#   Are my tests isolated?
+#   Can I reduce duplication in my test suite or implementation code?
+#   Can I make my implementation code more descriptive?
+#   Can I implement something more efficiently?
+
 RSpec.describe Category, type: :model do
-  before do
-    cat = Category.new("lifestyle")
+  # before(:each) do
+  #   @cat = Category.new(name: "lifestyle")
+  # end
+
+  subject { described_class.new }
+
+  context "when creating a new instance" do
+    it "is valid with valid attributes" do
+      subject.name = "lifestyle"
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
   end
 
   # use when, with, or without for context
   context ".name" do
     it "should return category name" do
-      expect(cat.name).to eq("lifestyle")
+      subject.name = "lifestyle"
+      expect(subject.name).to eq("lifestyle")
     end
   end
 
