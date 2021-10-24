77ce163232592103a85574e615d0e408ea29485c
grantkee <grant.d.kee@gmail.com>
2021-10-24 14:05:27 -0500

    set defaults and validations in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :expense
   has_one    :reocurring
+
+  attribute :amount, :integer, default: 0
+  attribute :transaction_date, :datetime, default: Time.now
+
+  validates_presence_of :name, :amount, :transaction_date
+
 end
